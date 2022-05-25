e801579b7675070597afdd98f3263ee1f3ad356b
alexey1254 <alxkill54@gmail.com>
2022-05-25 17:51:44 +0100

    se edita bbdd hospital

--- Tareas/Tercer Trimestre/BBDDHospital.sql
+++ Tareas/Tercer Trimestre/BBDDHospital.sql
@@ -61,9 +61,9 @@ CREATE TABLE Enfermo
 	NSS INT NULL
 );
 # Insertar datos en la table Dept
-INSERT INTO Dept(Dept_No,DNombre,Loc) VALUES(10,'CONTABILIDAD','ELCHE')
-INSERT INTO Dept(Dept_No,DNombre,Loc) VALUES(20,'INVESTIGACIÓN','MADRID')
-INSERT INTO Dept(Dept_No,DNombre,Loc) VALUES(30,'VENTAS','BARCELONA')
+INSERT INTO Dept(Dept_No,DNombre,Loc) VALUES(10,'CONTABILIDAD','ELCHE');
+INSERT INTO Dept(Dept_No,DNombre,Loc) VALUES(20,'INVESTIGACIÓN','MADRID');
+INSERT INTO Dept(Dept_No,DNombre,Loc) VALUES(30,'VENTAS','BARCELONA');
 INSERT INTO Dept(Dept_No,DNombre,Loc) VALUES(40,'PRODUCCIÓN','SALAMANCA');
 # Insertar datos en la tabla Emp
 INSERT INTO Emp( Emp_No, Apellido, Oficio, Dir, Fecha_Alt, Salario, Comision, Dept_No)
@@ -81,34 +81,34 @@ VALUES
 (7876,'ALONSO','EMPLEADO',7788,'03/05/1987',143000,0,20),
 (7900,'JIMENO','EMPLEADO',7698,'03/12/1981',123500,0,30),
 (7902,'FERNANDEZ','ANALISTA',7566,'03/12/1981',390000,0,20),
-(7934,'MU�OZ','EMPLEADO',7782,'23/06/1982',169000,0,10),
+(7934,'MUÑOZ','EMPLEADO',7782,'23/06/1982',169000,0,10),
 (7119,'SERRA','DIRECTOR',7839,'19/11/1983',225000,39000,20),
 (7322,'GARCIA','EMPLEADO',7119,'12/10/1982',129000,0,20);
 # Insertar datos en la tabla Hospital
-INSERT INTO Hospital(Hospital_Cod,Nombre,Direccion,Telefono, Num_Cama) VALUES(19,'Provincial','O Donell 50','964-4256',502)
-INSERT INTO Hospital(Hospital_Cod,Nombre,Direccion,Telefono, Num_Cama) VALUES(18,'General','Atocha s/n','595-3111',987)
-INSERT INTO Hospital(Hospital_Cod,Nombre,Direccion,Telefono, Num_Cama) VALUES(22,'La Paz','Castellana 1000','923-5411',412)
+INSERT INTO Hospital(Hospital_Cod,Nombre,Direccion,Telefono, Num_Cama) VALUES(19,'Provincial','O Donell 50','964-4256',502);
+INSERT INTO Hospital(Hospital_Cod,Nombre,Direccion,Telefono, Num_Cama) VALUES(18,'General','Atocha s/n','595-3111',987);
+INSERT INTO Hospital(Hospital_Cod,Nombre,Direccion,Telefono, Num_Cama) VALUES(22,'La Paz','Castellana 1000','923-5411',412);
 INSERT INTO Hospital(Hospital_Cod,Nombre,Direccion,Telefono, Num_Cama) VALUES(45,'San Carlos','Ciudad Universitaria','597-1500',845);
 # Insertar datos en la tabla Doctor
-INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(22,386,'Cabeza D.','Psiquiatría')
-INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(22,398,'Best D.','Urología')
-INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(19,435,'López A.','Cardiología')
-INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(22,453,'Galo D.','Pediatría')
-INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(45,522,'Adams C.','Neurología')
-INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(18,585,'Miller G.','Ginecología')
-INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(45,607,'Chuki P.','Pediatría')
+INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(22,386,'Cabeza D.','Psiquiatría');
+INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(22,398,'Best D.','Urología');
+INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(19,435,'López A.','Cardiología');
+INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(22,453,'Galo D.','Pediatría');
+INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(45,522,'Adams C.','Neurología');
+INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(18,585,'Miller G.','Ginecología');
+INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(45,607,'Chuki P.','Pediatría');
 INSERT INTO Doctor(Hospital_Cod,Doctor_No,Apellido,Especialidad) VALUES(18,982,'Cajal R.','Cardiología'); 
 # Insertar datos en la tabla Sala
-INSERT INTO SALA VALUES(1,22,'Recuperaci�n',10)
-INSERT INTO SALA VALUES(1,45,'Recuperaci�n',15)
-INSERT INTO SALA VALUES(2,22,'Maternidad',34)
-INSERT INTO SALA VALUES(2,45,'Maternidad',24)
-INSERT INTO SALA VALUES(3,19,'Cuidados Intensivos',21)
-INSERT INTO SALA VALUES(3,18,'Cuidados Intensivos',10)
-INSERT INTO SALA VALUES(4,18,'Cardiolog�a',53)
-INSERT INTO SALA VALUES(4,45,'Cardiolog�a',55)
-INSERT INTO SALA VALUES(6,19,'Psiqui�tricos',67)
-INSERT INTO SALA VALUES(6,22,'Psiqui�tricos',118);
+INSERT INTO Sala VALUES(1,22,'Recuperación',10);
+INSERT INTO Sala VALUES(1,45,'Recuperación',15);
+INSERT INTO Sala VALUES(2,22,'Maternidad',34);
+INSERT INTO Sala VALUES(2,45,'Maternidad',24);
+INSERT INTO Sala VALUES(3,19,'Cuidados Intensivos',21);
+INSERT INTO Sala VALUES(3,18,'Cuidados Intensivos',10);
+INSERT INTO Sala VALUES(4,18,'Cardiología',53);
+INSERT INTO Sala VALUES(4,45,'Cardiología',55);
+INSERT INTO Sala VALUES(6,19,'Psiquiátricos',67);
+INSERT INTO Sala VALUES(6,22,'Psiquiátricos',118);
 # Insertar datos en la tabla Plantilla
 INSERT INTO Plantilla(Hospital_Cod,Sala_Cod,Empleado_No,Apellido, Funcion, T, Salario)
 VALUES
@@ -125,7 +125,7 @@ VALUES
 # Insertar datos en la tabla Enfermo
 INSERT INTO Enfermo(Inscripcion,Apellido,Direccion,Fecha_Nac,S,NSS)
 VALUES
-(10995,'Lagu�a M.',;ya 20','16-may-56','M',280862422),
+(10995,'Lagua M.',Goya 20','16-may-56','M',280862422),
 (14024,'Fernández M.','Recoletos 50','21-may-60','F',284991452),
 (18004,'Serrano V.','Alcalá 12','23-jun-67','F',321790059),
 (36658,'Domin S.','Mayor 71','01-ene-42','M',160654471),
--- Tareas/Tercer Trimestre/Ej triggers 01.sql
+++ Tareas/Tercer Trimestre/Ej triggers 01.sql
@@ -2,6 +2,16 @@
 
 # 1) Crear un Trigger que borre en cascada sobre la tabla relacionada cuando borremos una sala. Mostrar el registro borrado al ejecutar el Trigger.
 
+DELIMITER //
+create trigger borradoCascada
+    before delete
+    on Sala
+    for each row
+BEGIN
+    
+end //
+
+DELIMITER ;
 
 
 # 2) Crear un Trigger que se active cuando Actualicemos alguna sala del hospital, modificando sus tablas relacionadas. Mostrar el registro Actualizado. 
