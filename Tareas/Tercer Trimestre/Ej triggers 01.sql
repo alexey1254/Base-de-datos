709b74d7918706e0f160b1578ff9967162353d57
alexey1254 <alxkill54@gmail.com>
2022-05-25 17:09:37 +0100

    Se añade bbdd hospital

---
+++ Tareas/Tercer Trimestre/Ej triggers 01.sql
@@ -0,0 +1,13 @@
+
+
+# 1) Crear un Trigger que borre en cascada sobre la tabla relacionada cuando borremos una sala. Mostrar el registro borrado al ejecutar el Trigger.
+
+
+
+# 2) Crear un Trigger que se active cuando Actualicemos alguna sala del hospital, modificando sus tablas relacionadas. Mostrar el registro Actualizado. 
+
+
+
+# 3) Crear un Trigger que se active al eliminar un registro en la tabla hospital y modifique las tablas correspondientes.
+
+
